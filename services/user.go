972f053e3c8baa28ce53780c921a7a81adcf144e
Aidan Rodriguez <aidan@aidanrodriguez.com>
2026-01-30 13:28:50 -0800

    found artist logging

--- services/user.go
+++ services/user.go
@@ -3,6 +3,7 @@ package services
 import (
 	"errors"
 	"fmt"
+	"log"
 	"maps"
 	"slices"
 
@@ -14,12 +15,14 @@ import (
 func IdentifyUser(db *gorm.DB, user models.User, songs []models.Song) (models.User, error) {
 	artistMap := make(map[uint]genius.Artist)
 	for _, song := range songs {
+		log.Printf("looking at %s\n", song.Title)
 		keyword := fmt.Sprintf("%s %s", song.Artist, song.Title)
 		fullName := fmt.Sprintf("%s %s", user.FirstName, user.LastName)
 		artists, err := genius.GeniusSearchArtists(keyword, fullName)
 		if err != nil {
 			continue
 		}
+		log.Printf("found %d artists \n", len(artists))
 		for _, artist := range artists {
 			artistMap[artist.ID] = artist
 		}
@@ -27,8 +30,12 @@ func IdentifyUser(db *gorm.DB, user models.User, songs []models.Song) (models.Us
 			break // prevent unnecessary loops
 		}
 	}
+
 	allArtists := slices.Collect(maps.Values(artistMap))
 	if len(allArtists) != 1 {
+		for artist := range allArtists {
+			log.Println("found ", artist)
+		}
 		return user, errors.New("too many matches please provide a different song")
 	}
 	user.GeniusID = &allArtists[0].ID
